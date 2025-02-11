74210577782331f2043fe4cd375497e1f6f8b181
SoulAuctioneer <ash.eldritch@gmail.com>
2025-02-11 02:38:53 -0800

    Slower decay

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ MIN_STROKE_SPEED = 0.25      # Minimum speed (position units per second)
 DIRECTION_REVERSAL_TOLERANCE = 0.05  # Tolerance for small direction reversals
 
 # Intensity tracking parameters
-INTENSITY_DECAY_RATE = 0.2   # Level lost per second
+INTENSITY_DECAY_RATE = 0.05   # Level lost per second
 INTENSITY_SPEED_FACTOR = 2.0  # Higher speeds reduce intensity gain (divisor)
 INTENSITY_DISTANCE_FACTOR = 1.0  # Multiplier for distance contribution
 
